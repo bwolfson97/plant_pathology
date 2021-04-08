1801e6d10ab8ff62b70e52634ac2e663d58771d7
Brandon Wolfson <bwolfson97@gmail.com>
2021-04-08 13:37:22 -0700

    Fix app

--- app/app.py
+++ app/app.py
@@ -6,14 +6,13 @@ https://ternaus.blog/tutorial/2020/08/28/Trained-model-what-is-next.html#vii-20-
 
 import numpy as np
 import streamlit as st
-from PIL import Image
+from fastai.vision.all import *
 from plant_pathology.pretrained_models import get_model
 
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
 
-@st.cache
 def cached_model():
     return get_model("resnet18_2021-04-07")
 
@@ -25,7 +24,8 @@ st.title("Classify leaf diseases")
 file = st.file_uploader("Upload an image...")
 
 if file is not None:
+    file = file.read()
     st.image(file)
     st.write("Classifying disease...")
-    preds = model.predict(file)
-    st.write(preds)
+    predicted_class, *_ = model.predict(file)
+    st.write(f"Predicted class: {predicted_class}")
